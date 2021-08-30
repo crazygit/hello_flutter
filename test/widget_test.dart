c171d417ad4313def9689a04a2439c989a297fa0
Crazygit <lianglin999@gmail.com>
2021-08-30 23:31:19 +0800

    add more demo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hello_flutter/MaterialWidgets/01main.dart';
+import 'package:hello_flutter/01MaterialWidgets/01main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
